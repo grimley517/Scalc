84a1e9609224194d0dca4b37180c5e0589a9a03d
Brian <brianjones@Eve.(none)>
2011-10-09 15:19:14 +1000

    Initial Commit

---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Brian Jones on 9/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
