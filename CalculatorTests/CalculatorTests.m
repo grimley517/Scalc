3ebd9bb2b6cca637ea22d1f64d551cdb0b9aef60
grimley517 <ba.jones@me.com>
2014-08-29 22:27:15 +1000

    updated a little

--- CalculatorTests/CalculatorTests.m
+++ CalculatorTests/CalculatorTests.m
@@ -23,13 +23,33 @@
 
 -(void)test1
 {
-    //simple addition test 1=2 = 3
+    //simple addition test 1+2 = 3
     [Calc setOperand:1];//simulates pressing the 1 button
     [Calc performOperation:@"+"]; //simulates pressing the + button
     [Calc setOperand:2]; //simulates pressing the 2 button
     double result = [Calc performOperation:@"="];//result should be returned when the equal sign pressed
     double expected = 3;
     XCTAssertEqual(expected, result , @"Expected 1+2 = 3");
+}
+
+-(void)test2
+{
+    //simple division test 1/2 = 0.5  not too different from test 1, but tests another infix function, and a proper double produced (naote this may be affected by weird decimalto binary effects.
+    [Calc setOperand:1];//simulates pressing the 1 button
+    [Calc performOperation:@"/"]; //simulates pressing the + button
+    [Calc setOperand:2]; //simulates pressing the 2 button
+    double result = [Calc performOperation:@"="];//result should be returned when the equal sign pressed
+    double expected = 0.5;
+    XCTAssertEqual(expected, result , @"Expected 1/2 = 0.5");
+}
+
+-(void)test3
+{
+    //simple negation - ie check the negation button, also checks that negative numbers display correctly
+    [Calc setOperand:1];//simulates pressing the 1 button
+    double result = [Calc performOperation:@"+/-"];//result should be returned when the equal sign pressed
+    double expected = -1;
+    XCTAssertEqual(expected, result , @"Expected is negative 1");
 }
 
 @end
